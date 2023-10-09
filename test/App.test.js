484a0cd18ea556fa425db9af58222c000c91d57d
ljohn177 <122895883+ljohn177@users.noreply.github.com>
2023-10-09 12:36:50 -0400

    Delete test directory

--- test/App.test.js
+++
@@ -1,9 +0,0 @@
-
-import React from 'react'
-import {render} from '@testing-library/react'
-import App from "./frontend/src/App"
-
-it("renders without crashing", () => {
-    shallow(<App />);
-  });
-  
