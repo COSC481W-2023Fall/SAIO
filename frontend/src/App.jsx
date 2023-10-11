2ccc4528a1bc9d425da468f9d166f37172e4b129
Deren B <47374311+DerenB@users.noreply.github.com>
2023-10-11 18:04:13 -0400

    Adding a jsx test file and importing to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 {/* Import Style Sheets */}
 import './App.css'
 
+{/* Importing the items in Components directory */}
+import { BasicTemplateComponent } from './components';
+
 {/* Importing the items in Pages directory */}
 import { BasicTemplatePage } from './pages';
 
@@ -8,6 +11,7 @@ function App() {
   return (
     <div>
       <p className='underline text-4xl'>Hello World</p>
+      <BasicTemplateComponent />
       <BasicTemplatePage />
     </div>
   )
--- frontend/src/components/index.jsx
+++ frontend/src/components/index.jsx
@@ -1,2 +1,1 @@
 export { default as BasicTemplateComponent } from './BasicTemplateComponent';
-export { default as Sum } from './sum';
