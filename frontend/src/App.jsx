dd709127c14c8c042265b61f298fc53976166a67
DerenB <dbozerprogramming@gmail.com>
2023-10-18 23:58:22 -0400

    Fixing error in App.jsx
    Path to style page needed updating to new location. Added a path for the default "/"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,9 @@ import Flashcards from './components/pages/Flashcards'
 import Home from './components/pages/Home'
 import Notes from './components/pages/Notes'
 import Todo from './components/pages/Todo'
+
 {/* Import Style Sheets */}
-import './App.css'
+import './style/App.css'
 
 {/* Importing the items in Components directory */}
 import { BasicTemplateComponent } from './components';
@@ -18,6 +19,7 @@ import { BasicTemplatePage } from './pages';
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Home/>}></Route>
       <Route path="/app/home" element={<Home/>}></Route>
       <Route path="/app/todo" element={<Todo/>}></Route>
       <Route path="/app/calendar" element={<Calendar/>}></Route>
