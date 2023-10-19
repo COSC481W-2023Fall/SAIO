fdc1cb6d2e9e51a75100875d48164766d3d44cbd
bpain96 <bcytlak@emich.edu>
2023-10-19 16:29:14 -0400

    New Todo App Function

--- frontend/src/components/pages/Todo.jsx
+++ frontend/src/components/pages/Todo.jsx
@@ -1,9 +1,17 @@
 import Sidebar from "../sidebar/Sidebar"
-
+import './TodoComponets/TodoApp.css'
+import TodoList from './TodoComponets/TodoList'
+/*Created with the Help of Brian Degins video and recourses
+    YouTube Channel : https://www.youtube.com/@briandesign
+    Video: https://www.youtube.com/watch?v=E1E08i2UJGI&list=LL&index=3
+    */
 export default function Todo() {
     return (
         <div>
-            <Sidebar/>
+            <Sidebar />
+            <div className='todo-app'>
+                <TodoList />
+            </div >
         </div>
     )
 }
