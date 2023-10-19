4010fee6283304b54b040f2607029bced7133536
DerenB <dbozerprogramming@gmail.com>
2023-10-18 22:46:39 -0400

    Adding Title to navbar links
    Added a title tag to the links in the navbar sidebar. This adds a hover effect to the cursor

--- frontend/src/components/sidebar/NavTab.jsx
+++ frontend/src/components/sidebar/NavTab.jsx
@@ -2,7 +2,7 @@ export default function NavTab(props) {
     let iconSource = `/images/icons/apps/${props.appName}.png`;
     return (
         <div className="navtab">
-            <a href={"/app/" + props.appName}>
+            <a href={"/app/" + props.appName} title={props.appName}>
                 <img src={iconSource} alt={props.appName}/>
             </a>
         </div>
