dbaa12f29f1b520690761bf46d20296b00a0f6bb
ljohn177 <122895883+ljohn177@users.noreply.github.com>
2023-10-22 11:21:56 -0400

    fix: Remove unnecessary code
    Remove unneeded code to make it more readable

--- backend/app.py
+++ backend/app.py
@@ -42,38 +42,3 @@ async def get_email(email: str):
 async def get_password(password: str):
    response = await check_password(password)
    return response
-'''''
-# Create User
-@app.post("/api/user", response_model=UserName)
-async def post_user(user:UserName):
-    response = await create_user(user.dict())
-    if response:
-        return response
-    raise HTTPException(400, "Something went wrong, bad request")
-
-# Read 1 User
-@app.get("/api/user{id}", response_model=UserName)
-async def get_user_by_id(id):
-    response = await read_one_user(id)
-    if response:
-        return response
-    raise HTTPException(404, f"There is no User with this ID: {id}")
-
-
-# Update User
-@app.put("/api/user/{id}", response_model=UserName)
-async def put_user(id:str, name:str):
-    response = await update_user(id, name)
-    if response:
-        return response
-    raise HTTPException(404, f"There is no Todo item with this title {id}")
-
-# Delete User
-@app.delete("/api/user/{id}/")
-async def delete_user(id):
-    response = await remove_user(id)
-    if response:
-        return "Successfully deleted User"
-    raise HTTPException(404, f"There is no Todo item with this title {id}")
-
-'''
