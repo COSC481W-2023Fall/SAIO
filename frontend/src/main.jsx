d11488e6de1c931a3a2698f4ca6bcc62ff13bdff
DerenB <dbozerprogramming@gmail.com>
2023-10-23 12:14:31 -0400

    Fixing routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -16,8 +16,6 @@ import { ContextProvider } from './contexts/ContextProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
+    <App/>
   </React.StrictMode>,
 )
