51abe5007627c10173e4dec66e667c85e30991ae
DerenB <dbozerprogramming@gmail.com>
2023-10-23 12:13:33 -0400

    Updating app test

--- frontend/src/__tests__/App.test.jsx
+++ frontend/src/__tests__/App.test.jsx
@@ -3,6 +3,6 @@ import {render,screen} from '@testing-library/react'
 
 test("should have hello world", () => {
     render(<App />);
-    const message = screen.queryByText(/Content/i);
+    const message = screen.queryByText(/Home/i);
     expect(message).toBeVisible();
 })
