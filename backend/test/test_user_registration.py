0e4920b65e01da642228516edeaf12a42aaa3ddc
rliu13 <rliu5@emich.edu>
2023-10-23 16:20:01 -0400

    fix: Added New Assert
    Added new assert in test_user_registration to assert userid

--- backend/test/test_user_registration.py
+++ backend/test/test_user_registration.py
@@ -6,20 +6,21 @@ client = TestClient(app)
 # Define the user registration data
 user_registration_data = {
     "name": "Test",
-    "email": "test7777@example.com",
+    "email": "test1234@example.com",
     "password": "password"
 }
 
 
 def test_user_registration():
     # Use the TestClient to make a POST request to the user registration endpoint
-    response = client.post("/account", json=user_registration_data)
+    response = client.post("https://saio-backend-8b4k2.ondigitalocean.app/account", json=user_registration_data)
     
     # Assert that the HTTP response status code is as expected
-    assert response.status_code == 200  # Change this to the expected status code
+    assert response.status_code == 200  
     
-    # Assert the response content or structure as needed
-    # For example, you can check if the response message is as expected
+    # Assert the response content 
     response_json = response.json()
+    assert "user_id" in response_json
     assert response_json["message"] == "User registered successfully"
+
 
