a408196d0fcabbfb5f8944a6a88761da0da1ea3d
Deren B <47374311+DerenB@users.noreply.github.com>
2023-10-31 16:43:23 -0400

    Removed the strict requirement in APP

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -15,7 +15,5 @@ import './style/Navbar.css'
 import { ContextProvider } from './contexts/ContextProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
+  <App/>
 )
