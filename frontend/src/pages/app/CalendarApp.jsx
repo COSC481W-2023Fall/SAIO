b954f1dff9a48f047de051445eae6f617bcd6f07
Deren B <47374311+DerenB@users.noreply.github.com>
2023-11-11 17:31:28 -0500

    Copying the code into a new file
    Changed the name of the page to "CalendarApp" and copied the code over from "Calendar"

---
+++ frontend/src/pages/app/CalendarApp.jsx
@@ -0,0 +1,120 @@
+import React, { useState, useEffect } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import axios from 'axios';
+import Select from 'react-select';
+import DateTimePicker from 'react-datetime-picker';
+// Import React packages
+
+// Import Sidebar component
+import Sidebar from "../../components/sidebar/Sidebar";
+
+// Import DateTime picker styling
+import 'react-datetime-picker/dist/DateTimePicker.css';
+import 'react-calendar/dist/Calendar.css';
+import 'react-clock/dist/Clock.css';
+
+// Import React Icons
+import { BsFillArrowUpCircleFill, BsFillArrowDownCircleFill } from 'react-icons/bs';
+
+// Create Localizer
+const localizer = momentLocalizer(moment);
+
+export default function CalendarApp() {
+    // Variable for getting Database calendar events
+    const [dataList, setDataList] = useState([])
+
+    // UseEffect for getting database calendar events and setting to dataList
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000/app/calendar')
+        .then(res => {
+            setDataList(res.data)
+        })
+    }, []);
+
+    // Iterates over the dataList and converts the date string to a DateTime
+    const appointments = dataList.map(appointment => ({
+        title: appointment.title,
+        start: new Date(appointment.start),
+        end: new Date(appointment.end), 
+        dataList:{appointment},
+        allDay: appointment.allDay,
+        resource: appointment.resource
+    }))
+
+    // Create a new Event Item
+    const [title, setTitle] = useState("");
+    const [start, setStart] = useState(new Date());
+    const [end, setEnd] = useState(new Date());
+    const [allDay, setAllDay] = useState();
+    const [resource, setResource] = useState("");
+
+    // Category Resource options
+    const resourceOptions = [
+        { value: 'school', label: 'School' },
+        { value: 'work', label: 'Work' },
+        { value: 'home', label: 'Home' },
+        { value: 'other', label: 'Other' }
+    ]
+
+    // All Day Options
+    const allDayOptions = [
+        { value: true, label: "All Day" },
+        { value: false, label: "Hourly" }
+    ]
+
+    // Add new Event to the Database
+    const addEventHandler = () => {
+        axios.post('http://127.0.0.1:8000/calendar', {
+            'title': title,
+            'start': start,
+            'end': end,
+            'allDay': allDay.value,
+            'resource': resource.value
+        })
+        .then(res => console.log(res))
+    };
+
+    // Toggle Form Entry
+    const [formOpen, setFormOpen] = useState(false)
+
+    // Handle Form Toggle
+    const handFormOpenToggle = () => {
+        setFormOpen(!formOpen);
+    }
+
+    // Checkbox Statuses
+    const [schoolChecked, setSchoolChecked] = useState(true);
+    const [workChecked, setWorkChecked] = useState(true);
+    const [homeChecked, setHomeChecked] = useState(true);
+    const [otherChecked, setOtherChecked] = useState(true);
+
+    // Handle Checkbox Changing
+    const handleSchoolChange = () => {
+        setSchoolChecked(!schoolChecked);
+    };
+    const handleWorkChange = () => {
+        setWorkChecked(!workChecked);
+    };
+    const handleHomeChange = () => {
+        setHomeChecked(!homeChecked);  
+    };
+    const handleOtherChange = () => {
+        setOtherChecked(!otherChecked);
+    };
+
+    return (
+        <div className="flex flex-row w-full">
+            <Sidebar/>
+            <div className='flex flex-col' id='main-calendar-section'>
+                <div>
+
+                </div>
+                <div>
+                    <div>Calendar</div>
+                    <div>Events List</div>
+                </div>
+            </div>
+        </div>
+    )
+}
