bd435ceaf833bf5e8e2ec026450a35d05e0cd4de
Sam <sstone20@emich.edu>
2023-11-15 18:57:26 -0500

    feat: unit test changes

--- frontend/src/__tests__/Notes.test.jsx
+++ frontend/src/__tests__/Notes.test.jsx
@@ -1,13 +1,13 @@
 import Notes from '../pages/app/Notes.jsx';
 import {render,screen} from '@testing-library/react';
 
-test("Save Notes Button Should Be Rendered", () => {
+test("Save Notes Button Should Be Rendered,", () => {
     render(<Notes />);
     const message = screen.queryByText(/Save/i); // add new adjacent
     expect(message).toBeVisible();
 })
 
-test("Add Adjacent Notes Button Should Be Rendered", () => {
+test("Add Adjacent Notes Button Should Be Rendered.", () => {
     render(<Notes />);
     const message = screen.queryByText(/\+/i); // add new adjacent
     expect(message).toBeVisible();
