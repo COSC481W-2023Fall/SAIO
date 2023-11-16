6439f44abe8638f7ff03316a4bf59cb792218168
rliu13 <rliu5@emich.edu>
2023-11-15 19:28:01 -0500

    test: Added Test Files
    Added test files for flashcards in backend and frontend tests

--- frontend/src/__tests__/Flashcards.test.jsx
+++ frontend/src/__tests__/Flashcards.test.jsx
@@ -1,8 +1,20 @@
-import Flashcard from '../pages/app/Flashcards';
+import FlashcardM from '../pages/app/FlashcardManagment';
+import FlashcardD from '../pages/app/DisplayFlashcards';
 import {render,screen} from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom'
 
-test("should have hello world", () => {
-    render(<Flashcard />);
-    const message = screen.queryByText(/Flash Cards/i);
+test("show study flashcards", () => {
+    render( <BrowserRouter>
+        <FlashcardM />
+      </BrowserRouter>);
+    const message = screen.queryByText(/Study Flashcards/i);
+    expect(message).toBeVisible();
+})
+
+test("show manage flashcards", () => {
+    render( <BrowserRouter>
+        <FlashcardD />
+      </BrowserRouter>);
+    const message = screen.queryByText(/Manage Flashcards/i);
     expect(message).toBeVisible();
 })
