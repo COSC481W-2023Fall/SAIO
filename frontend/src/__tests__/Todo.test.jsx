1ad0cca33b08e06b851411fbffc0ba6ba8abf5c5
bpain96 <bcytlak@emich.edu>
2023-11-15 19:01:59 -0500

    Added Test For Frontend

--- frontend/src/__tests__/Todo.test.jsx
+++ frontend/src/__tests__/Todo.test.jsx
@@ -1,3 +1,5 @@
+import TodoList from '../components/todo/TodoList';
+import TodoItems from '../components/todo/TodoItems';
 import Todo from '../pages/app/Todo';
 import {render,screen} from '@testing-library/react';
 
@@ -6,3 +8,10 @@ test("should have hello world", () => {
     const message = screen.queryByText(/Task for today/i);
     expect(message).toBeVisible();
 })
+
+test("New Event Button should be rendered", () => {
+    render(<TodoList />);
+    const message = screen.queryByText(/Add Todo/i);
+    expect(message).toBeVisible();
+})
+
