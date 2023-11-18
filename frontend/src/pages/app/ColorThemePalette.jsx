a856427dea6c3db82c082c38b929b17efbebbce8
Deren B <47374311+DerenB@users.noreply.github.com>
2023-11-17 21:15:36 -0500

    Setting up the color palette page

---
+++ frontend/src/pages/app/ColorThemePalette.jsx
@@ -0,0 +1,17 @@
+import Sidebar from "../../components/sidebar/Sidebar"
+
+import ThemeButton from '../../components/ThemeButton';
+
+const ColorThemePalette = () => {
+  return (
+    <div className="flex primaryBackground">
+      <Sidebar />
+      <div>
+        
+      </div>
+      <ThemeButton />
+    </div>
+  )
+}
+
+export default ColorThemePalette
